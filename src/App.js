03fa3d2693e9e903170b8a4f0bfd4bcb51e66413
winjun1128 <hsj1128ty@naver.com>
2025-09-15 11:27:55 +0900

    웹 포트폴리오 작성

--- src/App.js
+++ src/App.js
@@ -4,20 +4,45 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>OOO's Portfolio</h1>
+
+      <div className="port-container">
+        <div className="port-video">
+          <video src={"https://winjun1128.github.io/web-portfolio/videos/first.mp4"} autoPlay muted loop></video>
+        </div>
+        <div className="port-info">
+          <h3>주제 : 음식어쩌고</h3>
+          <p>일정 : </p>
+          <p>사용기술 : </p>
+          <p>설명 : </p>
+        </div>
+      </div>
+
+      <div className="port-container">
+        <div className="port-video">
+          <video src={"https://winjun1128.github.io/web-portfolio/videos/second.mp4"} autoPlay muted loop></video>
+        </div>
+        <div className="port-info">
+          <h3>주제 : 음식어쩌고</h3>
+          <p>일정 : </p>
+          <p>사용기술 : </p>
+          <p>설명 : </p>
+          <p>github Link : <a>https://github....</a></p>
+        </div>
+      </div>
+
+      <div className="port-container">
+        <div className="port-video">
+          <video src={"https://winjun1128.github.io/web-portfolio/videos/third.mkv"} autoPlay muted loop></video>
+        </div>
+        <div className="port-info">
+          <h3>주제 : 음식어쩌고</h3>
+          <p>일정 : </p>
+          <p>사용기술 : </p>
+          <p>설명 : </p>
+        </div>
+      </div>
+
     </div>
   );
 }
