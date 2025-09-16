70a36ae297342e1793948b5efbe515200f95ff32
winjun1128 <hsj1128ty@naver.com>
2025-09-16 10:36:19 +0900

    포트폴리오 사이트 대략 구현

--- src/App.js
+++ src/App.js
@@ -16,11 +16,12 @@ export default function App() {
           <video src="https://winjun1128.github.io/web-portfolio/videos/miniafterfirst.mp4" controls />
         </div>
         <div className="port-info">
-          <h3>주제 : 커뮤니티 기반 크라우드 펀딩 사이트</h3>
-          <p>📆일정 : 2025-05-28 ~ 2025-06-13</p>
-          <p>사용기술 : React</p>
-          <p>설명 : 기존 크라우드 펀딩과 다른 커뮤니티 기능을 추가해 판매자와 구매자의 소통 강화</p>
-          <p>깃허브 : <a href="https://github.com/winjun1128/CommunityCloudFunding">https://github.com/winjun1128/CommunityCloudFunding</a></p>
+          <h3>주제/이름 : 커뮤니티 기반 크라우드 펀딩/펀더스</h3>
+          <p><strong>📆일정 : </strong>2025-05-28 ~ 2025-06-13</p>
+          <p><strong>🛠사용기술 : </strong>React</p>
+          <p><strong>📢설명 : </strong>기존 크라우드 펀딩과 다른 커뮤니티 기능을 추가해 판매자와 구매자의 소통 강화</p>
+          <p><strong>깃허브 : </strong> <a href="https://github.com/winjun1128/CommunityCloudFunding">https://github.com/winjun1128/CommunityCloudFunding</a></p>
+          <p><strong>📜서류 : </strong> <a href="https://drive.google.com/drive/folders/1suBwzDi1O1N3CcVDS6T5OsEaDoPXSQed?usp=drive_link">https://drive.google.com/drive/folders/1suBwzDi1O1N3CcVDS6T5OsEaDoPXSQed?usp=drive_link</a></p>
           <p>
             <button
               onClick={() => setShowRole1(v => !v)}
@@ -38,7 +39,7 @@ export default function App() {
             <video src="https://winjun1128.github.io/web-portfolio/videos/minimyfirst.mp4" controls />
           </div>
           <div className="port-info">
-            <p>역할 : 홈, 리스트, 상품, 게시판 페이지 구현 및 팀 리드</p>
+            <p><strong>역할 : </strong>홈, 리스트, 상품, 게시판 페이지 구현 및 팀 리드</p>
           </div>
         </div>
       </div>
@@ -49,10 +50,12 @@ export default function App() {
           <video src="https://winjun1128.github.io/web-portfolio/videos/miniaftersecond.mp4" controls />
         </div>
         <div className="port-info">
-          <h3>주제 : 기업 스케줄 및 할일 관리 서비스</h3>
-          <p>📆일정 : 2025-07-28 ~ 2025-08-14</p>
-          <p>사용기술 : Spring framework, Oracle</p>
-          <p>깃허브 : <a href="https://github.com/winjun1128/BizBoard">https://github.com/winjun1128/BizBoard</a></p>
+          <h3>주제/이름 : 기업 스케줄 및 할일 관리 서비스/비즈보드</h3>
+          <p><strong>📆일정 : </strong>2025-07-28 ~ 2025-08-14</p>
+          <p><strong>🛠사용기술 : </strong>Spring framework, Oracle</p>
+           <p><strong>📢설명 : </strong>소규모 스타트업에 맞춘 기업 스케줄 및 할일 관리 서비스로 캘린더, 간트, 알림, 권한, 메모 등 으로 효율적인 비지니스를 지원</p>
+          <p><strong>깃허브 : </strong> <a href="https://github.com/winjun1128/BizBoard">https://github.com/winjun1128/BizBoard</a></p>
+          <p><strong>📜서류 : </strong> <a href="https://drive.google.com/drive/folders/1aswtGctU1i_SB-BtqomMlT-nRzEEdoES?usp=drive_link">https://drive.google.com/drive/folders/1aswtGctU1i_SB-BtqomMlT-nRzEEdoES?usp=drive_link</a></p>
           <p>
             <button
               onClick={() => setShowRole2(v => !v)}
@@ -70,7 +73,7 @@ export default function App() {
             <video src="https://winjun1128.github.io/web-portfolio/videos/minimysecond.mp4" controls />
           </div>
           <div className="port-info">
-            <p>역할 : 작업관리, 메모, 알림 기능 설계·구현</p>
+            <p><strong>역할 : </strong>프로젝트페이지의 작업관리, 메모, 알림, 공지, 초대 기능 설계·구현</p>
           </div>
         </div>
       </div>
@@ -81,10 +84,12 @@ export default function App() {
           <video src="https://winjun1128.github.io/web-portfolio/videos/miniafterthird.mp4" controls />
         </div>
         <div className="port-info">
-          <h3>주제 : 주유소/전기차 충전소 정보 제공 및 추천</h3>
-          <p>📆일정 : 2025-08-21 ~ 2025-09-12</p>
-          <p>사용기술 : React, Spring framework, Oracle</p>
-          <p>깃허브 : <a href="https://github.com/winjun1128/oilmoney">https://github.com/winjun1128/oilmoney</a></p>
+          <h3>주제/이름 : 주유소/전기차 충전소 정보 제공 및 추천/파워업</h3>
+          <p><strong>📆일정 : </strong>2025-08-21 ~ 2025-09-12</p>
+          <p><strong>🛠사용기술 : </strong>React, Spring framework, Oracle</p>
+           <p><strong>📢설명 : </strong>주유소/전기차 충전소 정보 제공 및 현재위치 기준 목적지 추천 시스템</p>
+          <p><strong>깃허브 : </strong><a href="https://github.com/winjun1128/oilmoney">https://github.com/winjun1128/oilmoney</a></p>
+          <p><strong>📜서류 : </strong><a href="https://drive.google.com/drive/folders/1JUohMzJ4CzSZzFHrNzPOZ1BGD-aqGu3s?usp=drive_link">https://drive.google.com/drive/folders/1JUohMzJ4CzSZzFHrNzPOZ1BGD-aqGu3s?usp=drive_link</a></p>
           <p>
             <button
               onClick={() => setShowRole3(v => !v)}
@@ -102,7 +107,7 @@ export default function App() {
             <video src="https://winjun1128.github.io/web-portfolio/videos/minimythird.mp4" controls />
           </div>
           <div className="port-info">
-            <p>역할 : 경로 페이지 전담 및 주유소/충전소 페이지 일부 구현</p>
+            <p><strong>역할 : </strong>경로 페이지 전담 및 주유소/충전소 페이지 일부 구현</p>
           </div>
         </div>
       </div>
